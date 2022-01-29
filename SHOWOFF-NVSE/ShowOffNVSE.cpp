ca7ce6b0fa68fc1e62bddb3b84172287f6f34154
Demorome <69116996+Demorome@users.noreply.github.com>
2022-01-29 19:44:32 -0400

    Update ShowOffNVSE.cpp

--- SHOWOFF-NVSE/ShowOffNVSE.cpp
+++ SHOWOFF-NVSE/ShowOffNVSE.cpp
@@ -605,6 +605,7 @@ extern "C"
 		REG_CMD_FORM(GetSelectedItemRefSO)
 
 		REG_CMD(TestSafeExtract_OneArray)
+		REG_CMD(TestSafeExtract_OneNumber_OneOptionalString)
 
 		REG_CMD(Flt_Equals)	// not needed; xNVSE's eval already uses this for "==" operator.
 
